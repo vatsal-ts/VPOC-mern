0ab6eb35dbea5805ae1d2f199521fb1d0d7a8b7d
vatsal-ts <84033475+vatsal-ts@users.noreply.github.com>
2022-06-30 01:23:11 +0530

    changes to user actions,productList

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -5,7 +5,7 @@ class Register extends Component {
   constructor() {
     super();
     this.state = {
-      name: "",
+      username: "",
       email: "",
       password: "",
       password2: "",
@@ -21,7 +21,7 @@ class Register extends Component {
     e.preventDefault();
 
     const newUser = {
-      name: this.state.name,
+      username: this.state.username,
       email: this.state.email,
       password: this.state.password,
       password2: this.state.password2,
@@ -33,80 +33,77 @@ class Register extends Component {
   render() {
     const { errors } = this.state;
     return ( 
-      <div className="container">
-        <div className="row">
-          <div className="col s8 offset-s2">
-            <Link to="/login" className="btn-flat waves-effect">
-              <i className="material-icons left">keyboard_backspace</i> Back to
-              home
-            </Link>
-            <div className="col s12" style={{ paddingLeft: "11.250px" }}>
-              <h4>
-                <b>Register</b> below
-              </h4>
-              <p className="grey-text text-darken-1">
-                Already have an account? <Link to="/login">Log in</Link>
-              </p>
+        <section className="vh-80" style={{backgroundColor: '#eee',paddingLeft:"100px",paddingRight:"100px",paddingTop:"50px",paddingBottom:"50px"}}>
+        <div className="container h-75 my-10">
+          <div className="row d-flex justify-content-center align-items-center h-50">
+            <div className="col-lg-12 col-md-12">
+              <div className="card text-black" style={{borderRadius: '25px',height:"80%"}} >
+                <div className="card-body p-md-5">
+                  <div className="row justify-content-center">
+                    <div className="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
+                      <p className="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Sign up</p>
+
+
+                      <form className="mx-1 mx-md-4" noValidate onSubmit={this.onSubmit}>
+
+                        <div className="d-flex flex-row align-items-center mb-4">
+                          <i className="fas fa-user fa-lg me-3 fa-fw" />
+                          <div className="form-outline flex-fill mb-0">
+                            <input type="text" id="username" className="form-control" onChange={this.onChange} value={this.state.username} error={errors.username}/>
+                            <label className="form-label" htmlFor="username">Your UserName</label>
+                          </div>
+                        </div>
+
+                        <div className="d-flex flex-row align-items-center mb-4">
+                          <i className="fas fa-envelope fa-lg me-3 fa-fw" />
+                          <div className="form-outline flex-fill mb-0">
+                            <input type="email" id="email" className="form-control" onChange={this.onChange} value={this.state.email} error={errors.email}/>
+                            <label className="form-label" htmlFor="email">Your Email</label>
+                          </div>
+                        </div>
+
+                        <div className="d-flex flex-row align-items-center mb-4">
+                          <i className="fas fa-lock fa-lg me-3 fa-fw" />
+                          <div className="form-outline flex-fill mb-0">
+                            <input type="password" id="password" className="form-control" onChange={this.onChange} value={this.state.password} error={errors.password}/>
+                            <label className="form-label" htmlFor="password">Password</label>
+                          </div>
+                        </div>
+
+                        <div className="d-flex flex-row align-items-center mb-4">
+                          <i className="fas fa-key fa-lg me-3 fa-fw" />
+                          <div className="form-outline flex-fill mb-0">
+                            <input type="password" id="password2" className="form-control" onChange={this.onChange} value={this.state.password2} error={errors.password2}/>
+                            <label className="form-label" htmlFor="password2">Repeat your password</label>
+                          </div>
+                        </div>
+
+                        <div className="form-check d-flex justify-content-center mb-5">
+                          <input className="form-check-input me-2" type="checkbox" defaultValue id="form2Example3c" />
+                          <label className="form-check-label" htmlFor="form2Example3">
+                            I agree all statements in <a href="#!">Terms of service</a>
+                          </label>
+                        </div>
+                        <div className="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
+                          <button type="submit" className="btn btn-primary btn-lg">Register</button>
+                        </div>
+                      </form>
+                      <div class="mb-0 h6"  style={{textAlign: 'center'}}>Already have an account?<br/>
+                        <Link to="/login" className="btn-flat waves-effect">Login</Link>
+                      </div>
+                      
+
+                    </div>
+                    <div className="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
+                      <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-registration/draw1.webp" className="img-fluid" alt="Sample image" />
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
-            <form noValidate onSubmit={this.onSubmit}>
-              <div className="input-field col s12">
-                <input
-                  onChange={this.onChange}
-                  value={this.state.name}
-                  error={errors.name}
-                  id="name"
-                  type="text"
-                />
-                <label htmlFor="name">Name</label>
-              </div>
-              <div className="input-field col s12">
-                <input
-                  onChange={this.onChange}
-                  value={this.state.email}
-                  error={errors.email}
-                  id="email"
-                  type="email"
-                />
-                <label htmlFor="email">Email</label>
-              </div>
-              <div className="input-field col s12">
-                <input
-                  onChange={this.onChange}
-                  value={this.state.password}
-                  error={errors.password}
-                  id="password"
-                  type="password"
-                />
-                <label htmlFor="password">Password</label>
-              </div>
-              <div className="input-field col s12">
-                <input
-                  onChange={this.onChange}
-                  value={this.state.password2}
-                  error={errors.password2}
-                  id="password2"
-                  type="password"
-                />
-                <label htmlFor="password2">Confirm Password</label>
-              </div>
-              <div className="col s12" style={{ paddingLeft: "11.250px" }}>
-                <button
-                  style={{
-                    width: "150px",
-                    borderRadius: "3px",
-                    letterSpacing: "1.5px",
-                    marginTop: "1rem",
-                  }}
-                  type="submit"
-                  className="btn btn-large waves-effect waves-light hoverable blue accent-3"
-                >
-                  Sign up
-                </button>
-              </div>
-            </form>
           </div>
         </div>
-      </div>
+      </section>
     );
   }
 }
---
+++ client/src/components/footer/Footer.component.js
@@ -0,0 +1,83 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="bg-light text-center text-white">
+      {/* Grid container */}
+      <div className="container p-4 pb-0">
+        {/* Section: Social media */}
+        <section className="mb-4">
+          {/* Facebook */}
+          <a
+            className="btn btn-primary btn-floating m-1"
+            style={{ backgroundColor: "#3b5998" ,borderRadius:"2rem"}}
+            href="#!"
+            role="button"
+          >
+            <i className="fab fa-facebook-f" />
+          </a>
+          {/* Twitter */}
+          <a
+            className="btn btn-primary btn-floating m-1"
+            style={{ backgroundColor: "#55acee" ,borderRadius:"2rem"}}
+            href="#!"
+            role="button"
+          >
+            <i className="fab fa-twitter" />
+          </a>
+          {/* Google */}
+          <a
+            className="btn btn-primary btn-floating m-1"
+            style={{ backgroundColor: "#dd4b39" ,borderRadius:"2rem"}}
+            href="#!"
+            role="button"
+          >
+            <i className="fab fa-google" />
+          </a>
+          {/* Instagram */}
+          <a
+            className="btn btn-primary btn-floating m-1"
+            style={{ backgroundColor: "#ac2bac" ,borderRadius:"2rem"}}
+            href="#!"
+            role="button"
+          >
+            <i className="fab fa-instagram" />
+          </a>
+          {/* Linkedin */}
+          <a
+            className="btn btn-primary btn-floating m-1"
+            style={{ backgroundColor: "#0082ca" ,borderRadius:"2rem"}}
+            href="#!"
+            role="button"
+          >
+            <i className="fab fa-linkedin-in" />
+          </a>
+          {/* Github */}
+          <a
+            className="btn btn-primary btn-floating m-1"
+            style={{ backgroundColor: "#333333",borderRadius:"2rem" }}
+            href="#!"
+            role="button"
+          >
+            <i className="fab fa-github" />
+          </a>
+        </section>
+        {/* Section: Social media */}
+      </div>
+      {/* Grid container */}
+      {/* Copyright */}
+      <div
+        className="text-center p-3"
+        style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}
+      >
+        © 2020 Copyright:
+        <a className="text-white" href="https://mdbootstrap.com/">
+          MDBootstrap.com
+        </a>
+      </div>
+      {/* Copyright */}
+    </footer>
+  );
+};
+
+export default Footer;
