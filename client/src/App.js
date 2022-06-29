8c3afcd437dd86cee41c795971b70d1295e2e9a2
vatsal-ts <84033475+vatsal-ts@users.noreply.github.com>
2022-06-29 14:20:09 +0530

    Login and register pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,39 @@
 import logo from "./logo.svg";
 import "./App.css";
-import React from "react";
+import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import Navbar from "./components/navbar.component"
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./components/navbar.component";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
+
 function App() {
   return (
     <Router>
-      {/* <div className="container"> */}
-        <Navbar />
-        <br />
-        {/* <Route path="/" exact component={ExercisesList} />
-        <Route path="/edit/:id" component={EditExercise} />
-        <Route path="/create" component={CreateExercise} />
-        <Route path="/user" component={CreateUser} /> */}
-      {/* </div> */}
+      <Navbar />
+      <Routes>
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </Router>
   );
 }
+
+// function App() {
+//   return (
+//     <Router>
+//         <Navbar />
+//         <br />
+//         {/* <Route path="/" exact component={ExercisesList} />
+//         <Route path="/edit/:id" component={EditExercise} />
+//         <Route path="/create" component={CreateExercise} />
+//         <Route path="/user" component={CreateUser} /> */}
+//         {/* <Route exact path="/register" component={Register} /> */}
+//         <Route exact path="/login" component={Login} />
+//     </Router>
+//   );
+// }
 
 export default App;
