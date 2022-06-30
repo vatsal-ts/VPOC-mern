b88e7163366a349857af757a58598158dce3ca19
vatsal-ts <84033475+vatsal-ts@users.noreply.github.com>
2022-07-01 03:36:23 +0530

    dashboard implemented

--- client/src/pages/CreateProduct.js
+++ client/src/pages/CreateProduct.js
@@ -5,18 +5,11 @@ export default class CreateExercise extends Component {
   constructor(props) {
     super(props);
 
-    this.onChangeUsername = this.onChangeUsername.bind(this);
-    this.onChangeDescription = this.onChangeDescription.bind(this);
-    this.onChangeDuration = this.onChangeDuration.bind(this);
-    this.onChangeDate = this.onChangeDate.bind(this);
-    this.onSubmit = this.onSubmit.bind(this);
-
     this.state = {
-      username: '',
+      title: '',
       description: '',
-      duration: 0,
-      date: new Date(),
-      users: []
+      price: 0,
+      category : ""
     }
   }
 
@@ -36,43 +29,23 @@ export default class CreateExercise extends Component {
 
   }
 
-  onChangeUsername(e) {
-    this.setState({
-      username: e.target.value
-    })
-  }
-
-  onChangeDescription(e) {
-    this.setState({
-      description: e.target.value
-    })
-  }
-
-  onChangeDuration(e) {
-    this.setState({
-      duration: e.target.value
-    })
-  }
-
-  onChangeDate(date) {
-    this.setState({
-      date: date
-    })
-  }
+  onChange = (e) => {
+    this.setState({ [e.target.id]: e.target.value });
+  };
 
   onSubmit(e) {
     e.preventDefault();
 
-    const exercise = {
-      username: this.state.username,
+    const product = {
+      title: this.state.title,
       description: this.state.description,
-      duration: this.state.duration,
-      date: this.state.date
+      price : this.state.price,
+      category: this.state.category
     }
 
-    console.log(exercise);
+    console.log(product);
 
-    axios.post('http://localhost:5000/exercises/add', exercise)
+    axios.post('/exercises/add', exercise)
       .then(res => console.log(res.data));
 
     window.location = '/';
---
+++ client/src/pages/dashboard_original.js
@@ -0,0 +1,122 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import axios from "axios";
+import { logoutUser } from "../actions/authActions";
+import CardComponent from "../components/Cards/CardGrid.component";
+
+class Dashboard extends Component {
+  constructor(props) {
+    super(props);
+    // console.dir(props);
+
+    this.state = {
+      selled: [],
+      buyed: [],
+      toBeSold: [],
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get("/products/buyer/" + this.props.auth.user.id)
+      .then((response) => {
+        console.log(response.status);
+        this.setState({ buyed: response.data });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+
+    axios
+      .get("/products/seller/" + this.props.auth.user.id)
+      .then((responses) => {
+        console.log(responses.status);
+        this.setState({ selled: [], toBeSold: [] });
+        responses.data.forEach((response) => {
+          if (response.buyerid) {
+            this.setState({ selled: [...this.state.selled, response] });
+            // console.log(this.props.auth')
+            // console.log(response)
+          } else {
+            this.setState({ toBeSold: [...this.state.toBeSold, response] });
+            // console.log(response)
+          }
+        });
+        // console.log(responses.data)
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  productList(productlist) {
+    return productlist.map((currentProduct) => {
+      return <CardComponent product={currentProduct} />;
+    });
+  }
+
+
+  onLogoutClick = (e) => {
+    e.preventDefault();
+
+    this.props.logoutUser();
+  };
+
+  render() {
+    const { user } = this.props.auth;
+    return (
+      <div style={{ height: "100vh" }} className="container valign-wrapper">
+        <div className="row">
+          <div className="col s12 center-align">
+            <h4>
+              <b>Hey there,</b> {user.username}
+              <p className="flow-text grey-text text-darken-1">
+                You are logged into a full-stack{" "}
+                <span style={{ fontFamily: "monospace" }}>MERN</span> app 👏
+              </p>
+            </h4>
+            <button
+              style={{
+                width: "150px",
+                borderRadius: "3px",
+                letterSpacing: "1.5px",
+                marginTop: "1rem",
+              }}
+              onClick={this.onLogoutClick}
+              className="btn btn-large waves-effect waves-light hoverable blue accent-3"
+            >
+              Logout
+            </button>
+          </div>
+        </div>
+        <div>selled</div>
+        <div
+          className="row row-cols-1 row-cols-md-5 g-4"
+          style={{ padding: "30px" }}
+        >
+          {this.productList(this.state.selled)}
+
+          <div>buyed</div>
+
+          {this.productList(this.state.buyed)}
+
+          <div>toBeSold</div>
+
+          {this.productList(this.state.toBeSold)}
+        </div>
+      </div>
+    );
+  }
+}
+
+Dashboard.propTypes = {
+  logoutUser: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+});
+
+export default connect(mapStateToProps, { logoutUser })(Dashboard);
