06c03a3c28e14200e2abb28275de4343fd207950
vatsal-ts <84033475+vatsal-ts@users.noreply.github.com>
2022-07-03 14:00:09 +0530

    searhc bar done

--- client/src/components/navbar/navbar.component.js
+++ client/src/components/navbar/navbar.component.js
@@ -53,105 +53,131 @@ class Navbar extends Component {
   inputHandler = (e) => {
     //convert input text to lower case
     this.setState({
-      searchList: this.state.allProducts.filter(
-        (product) =>
-          product.title.toLowerCase().charAt(0) ===
-            e.target.value.toLowerCase().charAt(0) &&
-          product.title.toLowerCase().includes(e.target.value.toLowerCase())
-      ),
+      searchList:
+        e.target.value === ""
+          ? []
+          : this.state.allProducts.filter(
+              (product) =>
+                product.title.substring(0, e.target.value.length) ===
+                e.target.value.toLowerCase()
+            ),
     });
   };
 
   render() {
     return (
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <div className="container-fluid">
-          {/* insert profile pic for navigating to users profile page */}
-          {this.state.profileImage ? (
-            <Link
-              to="/dashboard"
-              style={{
-                textDecoration: "none !important",
-                color: "none !important",
-              }}
+      <div>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <div className="container-fluid">
+            {/* insert profile pic for navigating to users profile page */}
+            {this.state.profileImage ? (
+              <Link
+                to="/dashboard"
+                style={{
+                  textDecoration: "none !important",
+                  color: "none !important",
+                }}
+              >
+                <i class="fa fa-user-circle-o">
+                  <AccountCircleIcon />
+                </i>
+              </Link>
+            ) : (
+              ""
+            )}
+            <h5 style={{ margin: "0.5rem" }}>
+              <a href="/">VPOC</a>
+            </h5>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
             >
-              <i class="fa fa-user-circle-o">
-                <AccountCircleIcon />
-              </i>
-            </Link>
-          ) : (
-            ""
-          )}
-          <h5 style={{ margin: "0.5rem" }}>
-            <a href="/">VPOC</a>
-          </h5>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <i class="fa-solid fa-bars"></i>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="/">
-                  Home
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  About
-                </a>
-              </li>
-              <li className="nav-item dropdown">
-                <a
-                  className="nav-link dropdown-toggle"
-                  href="#"
-                  id="navbarDropdown"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  Categories
-                </a>
-                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                  {this.state.categorys.map((cat) => (
-                    <li>
-                      <a
-                        className="dropdown-item"
-                        href={`/products/category/${cat.category}`}
-                      >
-                        {cat.category}
-                      </a>
-                    </li>
-                  ))}
-                </ul>
-              </li>
-            </ul>
-            <form className="d-flex">
-              <div class="searchbar">
-                <input
-                  class="search_input"
-                  type="text"
-                  name=""
-                  onChange={this.inputHandler}
-                  placeholder="Search..."
-                />
-                <a href="#" class="search_icon">
-                  <i class="fa fa-search"></i>
-                </a>
-              </div>
-              {this.state.searchList.map((product) => product.title + " ")}
-              {/* <SpecList input={this.state.inputText} /> */}
-            </form>
+              <i class="fa-solid fa-bars"></i>
+            </button>
+            <div
+              className="collapse navbar-collapse"
+              id="navbarSupportedContent"
+            >
+              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                <li className="nav-item">
+                  <a className="nav-link active" aria-current="page" href="/">
+                    Home
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    About
+                  </a>
+                </li>
+                <li className="nav-item dropdown">
+                  <a
+                    className="nav-link dropdown-toggle"
+                    href="#"
+                    id="navbarDropdown"
+                    role="button"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  >
+                    Categories
+                  </a>
+                  <ul
+                    className="dropdown-menu"
+                    aria-labelledby="navbarDropdown"
+                  >
+                    {this.state.categorys.map((cat) => (
+                      <li>
+                        <a
+                          className="dropdown-item"
+                          href={`/products/category/${cat.category}`}
+                        >
+                          {cat.category}
+                        </a>
+                      </li>
+                    ))}
+                  </ul>
+                </li>
+              </ul>
+              <form className="d-flex">
+                <div class="searchbar">
+                  <input
+                    class="search_input"
+                    type="text"
+                    name=""
+                    onChange={this.inputHandler}
+                    placeholder="Search..."
+                  />
+                  <a href="#" class="search_icon">
+                    <i class="fa fa-search"></i>
+                  </a>
+                </div>
+                {/* <SpecList input={this.state.inputText} /> */}
+              </form>
+            </div>
           </div>
-        </div>
-      </nav>
+        </nav>
+        {this.state.searchList.length === 0 ? (
+          ""
+        ) : (
+          <nav className="navbar navbar-expand-md bg-light navbar-dark">
+            <div className="navbar-collapse collapse pt-2 pt-md-0" id="navbar2">
+              <ul class="list-group-small list-group-flush list-group-light list-group-small">
+                {this.state.searchList.map((product) => (
+                  <li class="list-group-item bg-transparent">
+                    <a className="" href={`/products/product/${product._id}`}>
+                      {product.title}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </nav>
+        )}
+      </div>
     );
   }
 }
